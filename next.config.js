e53e14c34ac59374545ceec932423dd89c8ea499
matthewglasser <anika4dev@gmail.com>
2025-10-30 21:13:13 -0400

    video redirect - Matthew

--- next.config.js
+++ next.config.js
@@ -2,5 +2,13 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {};
+config.redirects = async () => [
+  {
+    source: "/demo",
+    destination:
+      "https://drive.google.com/file/d/14p9SIlg4THD7ZAf_VMkMwloLCL8pJDIt/view?usp=sharing",
+    permanent: true,
+  },
+];
 
 export default config;
